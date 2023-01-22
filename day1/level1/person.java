be2cd50b8674d1b8813965f3d2c1350c2f3d164a
VANSHAJ DHAR <himanshudhar6@gmail.com>
2023-01-22 10:59:34 +0530

    Update person.java

--- day1/level1/person.java
+++ day1/level1/person.java
@@ -1,5 +1,5 @@
 public class person{
-	static String name = "" ;
+	String name;
 	int age;
 	static int legs = 2;
 
@@ -11,3 +11,23 @@ public class person{
 		System.out.println("bye from "+ this.name);
 	}
 }
+
+person hrithik = new person();
+hrithik.name = "hrithik";
+hrithik.age = 22;
+hrithik.printLegs(); // 2
+
+person vanshaj = new person();
+vanshaj.name = "vanshaj";
+vanshaj.age = 30;
+vanshaj.printLegs(); // 2
+
+hrithik.legs = 5;
+hrithik.printLegs(); //5
+vanshaj.printLegs(); //5
+
+person vipul = new person();
+vipul.printLegs(); 5
+vipul.legs = 3;
+
+vanshaj.printLegs(); //3
